5f5c48332eb266c9bdb6f5e97feb6b8785e252ec
AndreyDol <113788568+AndreyDol@users.noreply.github.com>
2023-04-28 19:33:54 +0300

    add save to loadstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -52,6 +52,24 @@ export class App extends Component {
     return filterName;
   };
 
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),  
+      })
+    } else  {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+}
+  
   render() {
     const { filter } = this.state;
 
